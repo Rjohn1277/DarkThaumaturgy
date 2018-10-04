f2a4ee84df4a85e7cb997be5085de1fe1c5510a3
Ryan John <rjohn1277@gmail.com>
2018-10-04 10:33:20 -0400

    More Box2D stuff

--- DarkThaumaturgy/core/src/Screens/MainGameScreen.java
+++ DarkThaumaturgy/core/src/Screens/MainGameScreen.java
@@ -1,12 +1,16 @@
 package Screens;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.physics.box2d.BodyDef;
 import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
+import com.badlogic.gdx.physics.box2d.CircleShape;
+import com.badlogic.gdx.physics.box2d.FixtureDef;
 import com.badlogic.gdx.physics.box2d.World;
 import com.darkthaumaturgy.DarkThaumaturgy;
 
@@ -34,6 +38,23 @@ public class MainGameScreen implements Screen{
         gravitationalForces = new Vector2(0, -9.8f);
 
         world = new World(gravitationalForces, false);
+        b2dr = new Box2DDebugRenderer();
+        camera = new OrthographicCamera();
+        camera.setToOrtho(false, Gdx.graphics.getWidth(),Gdx.graphics.getHeight());
+        camera.position.set(camera.viewportWidth/2,camera.viewportHeight/2,0);
+
+    }
+
+    public Body createBody(Vector2 position, float size) {
+        Body body;
+        BodyDef bdef = new BodyDef();
+        FixtureDef fdef = new FixtureDef();
+
+        bdef.type = BodyDef.BodyType.DynamicBody;
+        bdef.position.set(camera.viewportWidth/2,camera.viewportHeight);
+
+        CircleShape shape = new CircleShape();
+        shape.setRadius(size/2);
     }
 
     @Override
