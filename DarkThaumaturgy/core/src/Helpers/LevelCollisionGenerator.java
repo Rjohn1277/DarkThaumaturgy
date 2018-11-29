7ebf47c037dbf6b0fc29f039163a28185c973d8e
Ryan John <rjohn1277@gmail.com>
2018-11-29 10:31:23 -0500

    Started Coding for TileSet videos

--- DarkThaumaturgy/core/src/Helpers/LevelCollisionGenerator.java
+++ DarkThaumaturgy/core/src/Helpers/LevelCollisionGenerator.java
@@ -2,6 +2,12 @@ package Helpers;
 
 import com.badlogic.ashley.core.Entity;
 import com.badlogic.ashley.core.PooledEngine;
+import com.badlogic.gdx.maps.MapLayer;
+import com.badlogic.gdx.maps.MapObject;
+import com.badlogic.gdx.maps.objects.RectangleMapObject;
+import com.badlogic.gdx.maps.objects.TextureMapObject;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.Body;
 import com.badlogic.gdx.physics.box2d.BodyDef;
@@ -21,39 +27,71 @@ public class LevelCollisionGenerator {
     public static final String TAG = LevelCollisionGenerator.class.getSimpleName();
     private World world;
     private PooledEngine engine;
+    private TiledMap map;
+    private static final String COLLISION_LAYER = "COLLISION_LAYER";
 
     public LevelCollisionGenerator(World world, PooledEngine engine) {
         this.world = world;
         this.engine = engine;
     }
 
-    public Entity createCollisionLevel(Vector2 position, Vector2 dimensions,
-                                     BodyDef.BodyType type, int bodyType) {
-        Body body;
-        BodyDef bdef = new BodyDef();
-        FixtureDef fdef = new FixtureDef();
-        Entity levelEntity = engine.createEntity();
+    public Entity createCollisionLevel(TiledMap map) {
+        this.map = map;
 
+        MapLayer layer = map.getLayers().get(COLLISION_LAYER);
 
+        for(MapObject object: layer.getObjects()) {
+            LevelGeometry geometry = null;
 
-
+            if(object instanceof TextureMapObject) {
+                continue;
+            }
+            Shape shape;
+            BodyDef bdef = new BodyDef();
+            String type = object.getProperties().get("Type",String.class);
             switch(type) {
-                case StaticBody:
+                case "StaticBody":
                     bdef.type = BodyDef.BodyType.StaticBody;
                     break;
-                case DynamicBody:
+                case "DynamicBody":
                     bdef.type = BodyDef.BodyType.DynamicBody;
                     break;
-                case KinematicBody:
+                case "KinematicBody":
                     bdef.type = BodyDef.BodyType.KinematicBody;
                     break;
 
             }
 
 
+            if(object instanceof RectangleMapObject) {
+                geometry =
+            }
+
+
+
+        }
+
+
+
+
+
+
+
+
+        Body body;
+
+        FixtureDef fdef = new FixtureDef();
+        Entity levelEntity = engine.createEntity();
+
+
+
+
+
+
+
             bdef.gravityScale = 1;
 
-            Shape shape;
+
 
             switch(bodyType) {
                 case 0:
@@ -100,5 +138,33 @@ public class LevelCollisionGenerator {
 
             return levelEntity;
 
+    }
+
+    private LevelGeometry getRectangle(RectangleMapObject rectangleMapObject) {
+        Rectangle rectangle = rectangleMapObject.getRectangle();
+        PolygonShape polygon = new PolygonShape();
+
+        Vector2 size = new Vector2(rectangle.x+rectangle.width/2,rectangle.y+rectangle.height/2);
+        polygon.setAsBox(rectangle.width/2,rectangle.height/2,size,0.0f);
+
+
+
+
+                /*shape = new PolygonShape();
+        ((PolygonShape)shape).setAsBox(dimensions.x/2, dimensions.y/2);
+        bdef.position.set(position.x+dimensions.x/2,position.y+dimensions.y/2);
+        break;*/
+    }
+
+    public static class LevelGeometry {
+        private Shape shape;
+
+        public LevelGeometry(Shape shape) {
+            this.shape = shape;
+        }
+
+        public Shape getShape() {
+            return shape;
+        }
     }
 }
